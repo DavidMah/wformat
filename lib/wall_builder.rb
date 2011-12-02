20ee4f029761d33033b9e71ac73ade755cec263c
Conner Stenerson <ConnerStenerson@gmail.com>
2011-12-02 01:22:14 -0800

    Made WallBuilder into a class
    - Created function to make a composite of two images

--- lib/wall_builder.rb
+++ lib/wall_builder.rb
@@ -1,1 +1,13 @@
 # wall_builder.rb
+
+require 'RMagick'
+include Magick
+
+class WallBuilder
+  def makewall(image_one, image_two)
+    input = ImageList.new(image_one, image_two)
+    output = input.montage { self.geometry = Magick::Geometry.new(1920,1200,0,0) }
+    output.write(File.basename(image_one,".jpg") + "&" + File.basename(image_two,".jpg") + ".jpg")
+    output.display
+  end
+end
