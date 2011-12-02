d9642b937e3e028673f84f1a16eadad49d136660
David Mah <Mahhaha@gmail.com>
2011-12-02 01:14:45 -0800

    Set up basic option parsing for later with Wa

--- lib/wallmerge.rb
+++ lib/wallmerge.rb
@@ -1,1 +1,19 @@
-puts "required!"
+#!/usr/bin/env ruby
+require 'optparse'
+require 'wall_builder.rb'
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: wallmerge [options] image1 image2"
+
+  opts.on('-h', '--help', 'Display This Screen') do
+    puts opts.banner
+    exit
+  end
+end
+
+optparse.parse!
+
+wall_builder = WallBuilder.new
+wall_builder.construct(*ARGV)
