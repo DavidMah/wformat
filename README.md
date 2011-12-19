f772ff9bf034f53670ca540dc675422ac2c66a49
David Mah <mahhaha@gmail.com>
2011-12-19 07:59:10 -0500

    Command Line argument cleaning

--- README.md
+++ README.md
@@ -3,6 +3,23 @@
 wformat is a command line utility to format images as computer
 wallpapers
 
+## Usage
+
+### Merging Images
+Create a merged image:
+
+`wformat merge [IMAGE_ONE] [IMAGE_TWO]`
+
+The default expected dual monitor screen is actually 1920x1200. Change
+that using options
+
+`wformat merge --widthone=1600 --heightone=1200 --widthtwo=800
+--heighttwo=600 [IMAGE_ONE] [IMAGE_TWO]`
+
+Or give the merge your own output filename
+
+`wformat merge --title="turtles.jpg" [IMAGE_ONE] [IMAGE_TWO]`
+
 ## Dependencies
 
 RMagick(https://rubygems.org/gems/rmagick)
--- lib/wformat.rb
+++ lib/wformat.rb
@@ -2,38 +2,51 @@
 require 'optparse'
 require 'wall_interface'
 
+opts_banner = ""
 options = {}
 
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: wformat [options] image1 image2"
+  opts.banner = "Usage: wformat command [options] arguments"
+  opts_banner = opts.banner
 
   opts.on('-h', '--help', 'Display This Screen') do
-    puts opts.banner
+    puts "wformat is a command line utility to format images as computer wallpapers"
+    puts "commands:\n  merge"
+    puts opts
     exit
   end
 
-  opts.on('--widthone WIDTH', Integer, 'Left Monitor WIDTH') do |width|
+  opts.on('--widthone=WIDTH', Integer, 'Left Monitor WIDTH') do |width|
     options['width_one'] = width
   end
 
-  opts.on('--widthtwo WIDTH', Integer, 'Right Monitor WIDTH') do |width|
+  opts.on('--widthtwo=WIDTH', Integer, 'Right Monitor WIDTH') do |width|
+    magic << self
     options['width_two'] = width
   end
 
-  opts.on('--heightone HEIGHT', Integer, 'Left Monitor HEIGHT') do |height|
+  opts.on('--heightone=HEIGHT', Integer, 'Left Monitor HEIGHT') do |height|
     options['height_one'] = height
   end
 
-  opts.on('--heighttwo HEIGHT', Integer, 'Right Monitor HEIGHT') do |height|
+  opts.on('--heighttwo=HEIGHT', Integer, 'Right Monitor HEIGHT') do |height|
     options['height_two'] = height
+  end
+
+  opts.on('--title=TITLE', "Output filename") do |title|
+    options['title'] = title
   end
 end
 
-optparse.parse!
+begin
+  optparse.parse!
+  help_message if ARGV.empty?
+  command   = ARGV.shift
+  arguments = ARGV << options
 
-interface = WallInterface.new
-
-command   = ARGV.shift
-arguments = ARGV << options
-
-interface.send(command, arguments)
+  interface = WallInterface.new
+  interface.send(command, arguments)
+rescue => ex
+  puts opts_banner
+  exit
+end
